57da608b10824bf0811a20f4c27eae19b7cd93bb
Cory Koch <yunusabd786@gmail.com>
2012-06-21 19:51:29 -0400

    Initial Commit

---
+++ prayer_service.php
@@ -0,0 +1,249 @@
+http://www.islamicfinder.org/prayer_service.php?country=Usa&city=Cleveland&state=OH&zipcode=&latitude=41.4995&longitude=-81.6959&timezone=-5.0&HanfiShafi=1&pmethod=5&fajrTwilight1=&fajrTwilight2=&ishaTwilight=0&ishaInterval=0&dhuhrInterval=1&maghribInterval=1&dayLight=1&simpleFormat=xml&monthly=1&month=
+
+﻿<?xml version="1.0"?><prayer>
+	<city>Cleveland</city>
+	<country>Usa</country>
+	<website>www.IslamicFinder.org</website>
+	<date day="1" month="6" year="2012" week_day="Friday">
+		<fajr>4:16</fajr>
+		<sunrise>5:55</sunrise>
+		<dhuhr>1:25</dhuhr>
+		<asr>5:26</asr>
+		<maghrib>8:55</maghrib>
+		<isha>10:34</isha>
+	</date>
+	<date day="2" month="6" year="2012" week_day="Saturday">
+		<fajr>4:15</fajr>
+		<sunrise>5:54</sunrise>
+		<dhuhr>1:25</dhuhr>
+		<asr>5:26</asr>
+		<maghrib>8:56</maghrib>
+		<isha>10:35</isha>
+	</date>
+	<date day="3" month="6" year="2012" week_day="Sunday">
+		<fajr>4:15</fajr>
+		<sunrise>5:54</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:27</asr>
+		<maghrib>8:57</maghrib>
+		<isha>10:36</isha>
+	</date>
+	<date day="4" month="6" year="2012" week_day="Monday">
+		<fajr>4:14</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:27</asr>
+		<maghrib>8:57</maghrib>
+		<isha>10:37</isha>
+	</date>
+	<date day="5" month="6" year="2012" week_day="Tuesday">
+		<fajr>4:13</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:27</asr>
+		<maghrib>8:58</maghrib>
+		<isha>10:38</isha>
+	</date>
+	<date day="6" month="6" year="2012" week_day="Wednesday">
+		<fajr>4:13</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:27</asr>
+		<maghrib>8:59</maghrib>
+		<isha>10:39</isha>
+	</date>
+	<date day="7" month="6" year="2012" week_day="Thursday">
+		<fajr>4:12</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:28</asr>
+		<maghrib>8:59</maghrib>
+		<isha>10:40</isha>
+	</date>
+	<date day="8" month="6" year="2012" week_day="Friday">
+		<fajr>4:12</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:26</dhuhr>
+		<asr>5:28</asr>
+		<maghrib>9:00</maghrib>
+		<isha>10:41</isha>
+	</date>
+	<date day="9" month="6" year="2012" week_day="Saturday">
+		<fajr>4:12</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:27</dhuhr>
+		<asr>5:28</asr>
+		<maghrib>9:00</maghrib>
+		<isha>10:42</isha>
+	</date>
+	<date day="10" month="6" year="2012" week_day="Sunday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:27</dhuhr>
+		<asr>5:28</asr>
+		<maghrib>9:01</maghrib>
+		<isha>10:42</isha>
+	</date>
+	<date day="11" month="6" year="2012" week_day="Monday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:27</dhuhr>
+		<asr>5:29</asr>
+		<maghrib>9:01</maghrib>
+		<isha>10:43</isha>
+	</date>
+	<date day="12" month="6" year="2012" week_day="Tuesday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:27</dhuhr>
+		<asr>5:29</asr>
+		<maghrib>9:02</maghrib>
+		<isha>10:44</isha>
+	</date>
+	<date day="13" month="6" year="2012" week_day="Wednesday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:27</dhuhr>
+		<asr>5:29</asr>
+		<maghrib>9:02</maghrib>
+		<isha>10:44</isha>
+	</date>
+	<date day="14" month="6" year="2012" week_day="Thursday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:28</dhuhr>
+		<asr>5:30</asr>
+		<maghrib>9:03</maghrib>
+		<isha>10:45</isha>
+	</date>
+	<date day="15" month="6" year="2012" week_day="Friday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:28</dhuhr>
+		<asr>5:30</asr>
+		<maghrib>9:03</maghrib>
+		<isha>10:45</isha>
+	</date>
+	<date day="16" month="6" year="2012" week_day="Saturday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:28</dhuhr>
+		<asr>5:30</asr>
+		<maghrib>9:03</maghrib>
+		<isha>10:46</isha>
+	</date>
+	<date day="17" month="6" year="2012" week_day="Sunday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:28</dhuhr>
+		<asr>5:30</asr>
+		<maghrib>9:04</maghrib>
+		<isha>10:46</isha>
+	</date>
+	<date day="18" month="6" year="2012" week_day="Monday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:29</dhuhr>
+		<asr>5:30</asr>
+		<maghrib>9:04</maghrib>
+		<isha>10:46</isha>
+	</date>
+	<date day="19" month="6" year="2012" week_day="Tuesday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:29</dhuhr>
+		<asr>5:31</asr>
+		<maghrib>9:04</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="20" month="6" year="2012" week_day="Wednesday">
+		<fajr>4:11</fajr>
+		<sunrise>5:52</sunrise>
+		<dhuhr>1:29</dhuhr>
+		<asr>5:31</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="21" month="6" year="2012" week_day="Thursday">
+		<fajr>4:11</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:29</dhuhr>
+		<asr>5:31</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="22" month="6" year="2012" week_day="Friday">
+		<fajr>4:12</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:29</dhuhr>
+		<asr>5:31</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="23" month="6" year="2012" week_day="Saturday">
+		<fajr>4:12</fajr>
+		<sunrise>5:53</sunrise>
+		<dhuhr>1:30</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="24" month="6" year="2012" week_day="Sunday">
+		<fajr>4:12</fajr>
+		<sunrise>5:54</sunrise>
+		<dhuhr>1:30</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="25" month="6" year="2012" week_day="Monday">
+		<fajr>4:13</fajr>
+		<sunrise>5:54</sunrise>
+		<dhuhr>1:30</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="26" month="6" year="2012" week_day="Tuesday">
+		<fajr>4:13</fajr>
+		<sunrise>5:54</sunrise>
+		<dhuhr>1:30</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="27" month="6" year="2012" week_day="Wednesday">
+		<fajr>4:14</fajr>
+		<sunrise>5:55</sunrise>
+		<dhuhr>1:30</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="28" month="6" year="2012" week_day="Thursday">
+		<fajr>4:14</fajr>
+		<sunrise>5:55</sunrise>
+		<dhuhr>1:31</dhuhr>
+		<asr>5:32</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="29" month="6" year="2012" week_day="Friday">
+		<fajr>4:15</fajr>
+		<sunrise>5:56</sunrise>
+		<dhuhr>1:31</dhuhr>
+		<asr>5:33</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:47</isha>
+	</date>
+	<date day="30" month="6" year="2012" week_day="Saturday">
+		<fajr>4:16</fajr>
+		<sunrise>5:56</sunrise>
+		<dhuhr>1:31</dhuhr>
+		<asr>5:33</asr>
+		<maghrib>9:05</maghrib>
+		<isha>10:46</isha>
+	</date>
+</prayer>    
+    
+    
